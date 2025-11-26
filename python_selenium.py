793e26144e385f7f2e60e1100eea5aff22ddca1f
Alexis-vs <alexis.vanstraaten@outlook.fr>
2025-11-26 19:38:53 +0100

    Try to fix Selenium for Github Actions

--- python_selenium.py
+++ python_selenium.py
@@ -5,6 +5,7 @@ from io import StringIO
 from bs4 import BeautifulSoup
 from selenium import webdriver
 from selenium.webdriver.chrome.options import Options
+from selenium.webdriver.support.ui import WebDriverWait
 
 def extract_table_from_url(urls: list[str], table_id: str, image: bool = False) -> tuple[pd.DataFrame, str | None]:
     """
@@ -31,9 +32,17 @@ def extract_table_from_url(urls: list[str], table_id: str, image: bool = False) 
     options.add_argument('--no-sandbox')
     options.add_argument('--disable-dev-shm-usage')
     options.add_argument('--disable-blink-features=AutomationControlled')
+    options.add_argument("--lang=fr-FR")
     options.add_argument('--window-size=1920,1080')
+    options.add_argument("--disable-gpu")
+    options.add_argument("--disable-infobars")
+    options.add_argument("--disable-extensions")
+    options.add_argument("--disable-popup-blocking")
     options.add_argument('--user-agent=Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36')
     
+    options.add_argument("--disable-software-rasterizer")
+    options.add_argument("--disable-features=VizDisplayCompositor")
+
     options.add_experimental_option("excludeSwitches", ["enable-automation"])
     options.add_experimental_option('useAutomationExtension', False)
     
@@ -42,15 +51,26 @@ def extract_table_from_url(urls: list[str], table_id: str, image: bool = False) 
     driver.execute_cdp_cmd('Network.setUserAgentOverride', {
         "userAgent": 'Mozilla/5.0 (Macintosh; Intel Mac OS X 10_15_7) AppleWebKit/537.36 (KHTML, like Gecko) Chrome/120.0.0.0 Safari/537.36'
     })
+    
+    driver.execute_cdp_cmd("Page.addScriptToEvaluateOnNewDocument",{
+            "source": """
+                Object.defineProperty(navigator, 'webdriver', {
+                    get: () => undefined
+                });
+            """
+    })
+    
     driver.execute_script("Object.defineProperty(navigator, 'webdriver', {get: () => undefined})")
     
     results = []
     results_images = []
+    wait = WebDriverWait(driver, 15)
     
     for url in urls:
         try:
             driver.get(url)
-            time.sleep(20)
+            # time.sleep(20)
+            wait.until(lambda d: d.execute_script("return document.readyState") == "complete")
         
             html = driver.page_source
             html = re.sub(r'<!--', '', html)
